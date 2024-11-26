0ed7f57b91ec7296bd856e5a85018910c06bebb1
MYMEILE <529266369@qq.com>
2024-11-26 16:44:28 +0800

    add docker

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from app.tg_stream import TgStream
 async def main():
     tg = TgStream()
     result = await tg.run()
-    tree_news_result = await listen_to_tree_news()
     print(result)
 
 if __name__ == '__main__':
