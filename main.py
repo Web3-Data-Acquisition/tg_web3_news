5723314a92937fa4806b675d8c3ec37c02fa3f4d
MYMEILE <529266369@qq.com>
2024-11-26 17:51:48 +0800

    delete docker

--- main.py
+++ main.py
@@ -4,10 +4,22 @@ from app.services.tree_news_data import listen_to_tree_news
 from app.tg_stream import TgStream
 
 
-async def main():
+async def run_tg_stream():
     tg = TgStream()
     result = await tg.run()
-    print(result)
+
+
+async def run_tree_news():
+    tree_news_result = await listen_to_tree_news()
+
+
+async def main():
+    # 使用 asyncio.gather 来并行运行两个任务
+    await asyncio.gather(
+        run_tg_stream(),
+        run_tree_news()
+    )
+
 
 if __name__ == '__main__':
     asyncio.run(main())
