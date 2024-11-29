8f9b024b6e35832ba8cadf1d3e7e2bfbf7e4464e
MYMEILE <529266369@qq.com>
2024-11-29 19:10:56 +0800

    update gpt

--- main.py
+++ main.py
@@ -2,12 +2,12 @@ import asyncio
 
 # from app.services.get_moonshot_list import moonshot_main
 from app.services.tree_news_data import listen_to_tree_news
-from app.tg_stream import TgStream
+# from app.tg_stream import TgStream
 
 
-async def run_tg_stream():
-    tg = TgStream()
-    result = await tg.run()
+# async def run_tg_stream():
+#     tg = TgStream()
+#     result = await tg.run()
 
 
 async def run_tree_news():
@@ -21,7 +21,7 @@ async def run_tree_news():
 async def main():
     # 使用 asyncio.gather 来并行运行两个任务
     await asyncio.gather(
-        run_tg_stream(),
+        # run_tg_stream(),
         run_tree_news(),
         # moonshot_news()
     )
